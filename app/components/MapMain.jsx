c0bef4ea8e5b229b5615c743d417eb355ebc1745
ev-dev <evan.reed1100@gmail.com>
2017-07-20 15:50:41 -0400

    working on map component

---
+++ app/components/MapMain.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import topojson from 'topojson';
+import { MapChoropleth } from 'react-d3-map-choropleth';
+// import topodata from 'SOMEFILE'
+
+let width = 700, height = 500;
+let dataPrecincts = topojson.mesh()
+
+
+export default class MapMain extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (  
+
+    )
+  }
+}
