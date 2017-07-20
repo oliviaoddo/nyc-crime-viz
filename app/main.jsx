db3a187bd44c2b254f7535fa6552f19b57b1a91e
deepseafishing <onejunekang@gmail.com>
2017-07-19 22:40:57 -0400

    google map setting doen

--- app/main.jsx
+++ app/main.jsx
@@ -1,14 +1,14 @@
-'use strict'
-import React from 'react'
-import {render} from 'react-dom'
-import { Provider } from 'react-redux'
+'use strict';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
-import store from './store'
-import Root from './components/Root'
+import store from './store';
+import Root from './components/SimpleMapExampleGoogleMap';
 
-render (
-  <Provider store={store}>
-    <Root/>
-  </Provider>,
-  document.getElementById('main')
-)
+render(
+	<Provider store={store}>
+		<Root />
+	</Provider>,
+	document.getElementById('main')
+);
